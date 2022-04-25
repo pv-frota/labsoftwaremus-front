b58c3b5d46c82d5a71730dba3faf6236466c835e
pedro.victor <pvjacinto8@gmail.com>
2022-04-25 20:51:07 -0300

    configuração quasar

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -2,10 +2,50 @@
 import './styles/quasar.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import lang from 'quasar/lang/pt-BR.js'
+import {
+  ClosePopup,
+  QDialog,
+  Notify,
+  Dialog,
+  Loading,
+  QSpinnerGears,
+  LoadingBar,
+  Ripple,
+  QSpinnerHourglass
+} from 'quasar'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
+  directives: {
+    ClosePopup,
+    Ripple
+  },
   plugins: {
-  }
+    Dialog,
+    QDialog,
+    Notify,
+    Loading,
+    QSpinnerGears,
+    LoadingBar,
+    QSpinnerHourglass
+  },
+  animations: 'all',
+  config: {
+    notify: { /* Notify defaults */ },
+    loading: { /* Loading defaults */ }
+  },
+  lang: lang
 }
+
+Loading.setDefaults({
+  message: 'Carregando...',
+  spinner: QSpinnerGears,
+  spinnerColor: 'primary'
+
+})
+LoadingBar.setDefaults({
+  color: 'primary',
+  size: '10px',
+  position: 'bottom'
+})

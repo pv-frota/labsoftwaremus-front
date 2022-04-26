c9093cc3664299ec13103a4210e101951f4f2b97
pedro.victor <pvjacinto8@gmail.com>
2022-04-25 21:08:38 -0300

    tela de classificacao

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,98 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row justify-evenly">
+    <q-table
+      title="Classificações"
+      :rows="classificacoes"
+      :columns="columns"
+      row-key="id"
+      class="col-4"
+    >
+      <template v-slot:body-cell-acoes="props">
+        <q-td :props="props">
+          <q-btn
+            size="sm"
+            color="info"
+            dense
+            icon="fas fa-edit"
+            class="q-pa-xs"
+          >
+            <q-tooltip>
+              Editar
+            </q-tooltip>
+          </q-btn>
+          <q-btn
+            size="sm"
+            color="negative"
+            dense
+            icon="fas fa-trash"
+            class="q-pa-xs q-ml-xs"
+          >
+            <q-tooltip>
+              Excluir
+            </q-tooltip>
+          </q-btn>
+        </q-td>
+      </template>
+    </q-table>
+    <q-table
+      title="Classificações"
+      :rows="classificacoes"
+      :columns="columns"
+      row-key="id"
+      class="col-4"
+    >
+      <template v-slot:body-cell-acoes="props">
+        <q-td :props="props">
+          <q-btn
+            size="sm"
+            color="info"
+            dense
+            icon="fas fa-edit"
+            class="q-pa-xs"
+          >
+            <q-tooltip>
+              Editar
+            </q-tooltip>
+          </q-btn>
+          <q-btn
+            size="sm"
+            color="negative"
+            dense
+            icon="fas fa-trash"
+            class="q-pa-xs q-ml-xs"
+          >
+            <q-tooltip>
+              Excluir
+            </q-tooltip>
+          </q-btn>
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
+
+
+<script>
+import { columns } from '../constantes/classificacao.const'
+export default {
+  name: 'AboutView',
+  data () {
+    return {
+      classificacoes: [],
+      columns: columns
+    }
+  },
+  async mounted () {
+    try {
+      this.$q.loading.show()
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      let response = await this.$services.classificacao().list(true)
+      this.classificacoes = response.data
+    } catch (e) {
+      console.log(e)
+    } finally {
+      this.$q.loading.hide()
+    }
+  }
+}
+</script>

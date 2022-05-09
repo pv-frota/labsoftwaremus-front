b3c7b4c4963f02cd8bb556b5745ec3be82e19f60
Victor Santos <victor.vsantos08@gmail.com>
2022-05-09 13:10:57 -0300

    Update Interface
    Update no componente Header.
    Create do componente Footer.
    Inicio da interface da Home Page.

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,21 @@
     <q-page-container>
       <router-view/>
     </q-page-container>
+    <base-footer/>
   </q-layout>
 </template>
 
 
 <script>
+// importações
 import BaseHeader from '@/components/BaseHeader.vue'
+import BaseFooter from '@/components/BaseFooter.vue'
+
 export default {
   name: 'App',
   components: {
-    BaseHeader
+    BaseHeader,
+    BaseFooter,
   },
 }
 </script>

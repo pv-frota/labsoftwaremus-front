8308216cb7077917bec27629626ff6d035b07ed6
pedro.victor <pvjacinto8@gmail.com>
2022-05-16 20:43:43 -0300

    ajustes autocomplete

--- src/components/SelectAutocomplete.vue
+++ src/components/SelectAutocomplete.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="q-pa-md">
-    <div class="q-gutter-md row">
-      <q-select outlined dense stack-label
-        v-model="value"
-        v-bind="$attrs"
-        map-options emit-value
-        :label="label"
-        :options="values"
-        :option-label="attribute"
-      >
-        <template v-slot:no-option>
-          <q-item>
-            <q-item-section class="text-grey">
-              Sem resultados
-            </q-item-section>
-          </q-item>
-        </template>
-      </q-select>
-    </div>
-  </div>
+  <q-select outlined dense stack-label behavior="menu"
+    v-model="value"
+    v-bind="$attrs"
+    use-input
+    input-debounce="0"
+    @filter="filterFn"
+    :label="label"
+    :options="filtered"
+    :option-label="attribute"
+    :disable="disable"
+  >
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          Sem resultados
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
 </template>
 
 <script>
@@ -36,12 +35,37 @@ export default {
     attribute: {
       type: String,
       required: true
+    },
+    disable: {
+      type: Boolean,
+      required: false,
+      default: () => false
+    }
+  },
+  data () {
+    return {
+      filtered: this.values
     }
   },
   computed: {
     value: {
       get () { return this.$attrs.value },
-      set (val) { this.$emit('input', val) }
+      set (val) { this.$emit('select', val) }
+    }
+  },
+  methods: {
+    filterFn (val, update) {
+      if (val === '') {
+        update(() => {
+          this.filtered = this.values
+        })
+        return
+      }
+
+      update(() => {
+        const needle = val.toLowerCase()
+        this.filtered = this.values.filter(v => v[this.attribute].toLowerCase().startsWith(needle))
+      })
     }
   }
 }
